845b56b8d4ddf31ee6b94324dfea1da55601de85
jhs512 <jangka512@gmail.com>
2025-07-15 15:23:00 +0900

    5

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -10,6 +10,13 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  {
+    ignores: [
+      "src/components/**",
+      "src/hooks/**",
+      "src/lib/backend/*/schema.d.ts",
+    ],
+  },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
