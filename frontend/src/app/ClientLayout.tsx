100f1b7ac075ac87ff9344ff190a0797b5f55bd2
jhs512 <jangka512@gmail.com>
2025-07-17 10:35:24 +0900

    step 01

--- frontend/src/app/ClientLayout.tsx
+++ frontend/src/app/ClientLayout.tsx
@@ -1,57 +1,9 @@
 "use client";
 
-import type { components } from "@/global/backend/apiV1/schema";
-import client from "@/global/backend/client";
-import { createContext, useEffect, useState } from "react";
+import useAuth, { AuthContext } from "@/global/auth/hooks/useAuth";
 
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-
-type MemberDto = components["schemas"]["MemberDto"];
-
-export const AuthContext = createContext<ReturnType<typeof useAuth> | null>(
-  null,
-);
-
-function useAuth() {
-  const [loginMember, setLoginMember] = useState<MemberDto | null>(null);
-  const isLogin = loginMember !== null;
-
-  useEffect(() => {
-    client.GET("/api/v1/members/me").then((res) => {
-      if (res.error) return;
-
-      setLoginMember(res.data);
-    });
-  }, []);
-
-  const clearLoginMember = () => {
-    setLoginMember(null);
-  };
-
-  const logout = (onSuccess: () => void) => {
-    client.DELETE("/api/v1/members/logout").then((res) => {
-      if (res.error) {
-        alert(res.error.msg);
-        return;
-      }
-
-      clearLoginMember();
-
-      onSuccess();
-    });
-  };
-
-  if (isLogin)
-    return { isLogin: true, loginMember, logout, setLoginMember } as const;
-
-  return {
-    isLogin: false,
-    loginMember: null,
-    logout,
-    setLoginMember,
-  } as const;
-}
 
 export default function ClientLayout({
   children,
